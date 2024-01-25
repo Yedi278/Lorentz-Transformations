0020d6f286cf83e8cfd635930b0d038f72317be5
Yehan Edirisinghe <yedi278@gmail.com>
2024-01-25 22:52:23 +0100

    .

---
+++ LF.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+c = 3e8
+
+class transf():
+
+    def __init__(self,v):
+        self.v = v
+        self.betha, self.gamma = self.gamma(self.v)
+    
+    def lorentz_fact(v):
+
+        betha = v/c
+        gamma = 1/np.sqrt(1-np.power(betha,2))
+
+        return betha,gamma
+
+    def trasf(self,x,y,z,t):
+        x1 = (x - self.v*t)* self.gamma
+        t1 = self.gamma* (t - (self.v*x/c**2))
